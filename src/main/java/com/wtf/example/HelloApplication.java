fb6117ecc66b20f76a3b928b6e6a6553d7946d82
吴腾飞 <2933670738@qq.com>
2021-11-08 09:44:14 +0800

    test

--- src/main/java/com/wtf/example/HelloApplication.java
+++ src/main/java/com/wtf/example/HelloApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class LearnGitApplication {
+public class HelloApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(LearnGitApplication.class, args);
+        SpringApplication.run(HelloApplication.class, args);
     }
 
 }
